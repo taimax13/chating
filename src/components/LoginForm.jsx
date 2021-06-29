6b5d9164f4a076f7b6ef057442b71523cbea2838
talex.maxim <talex.maxim@pyramidanalytics.com>
2021-06-29 15:53:53 +0300

    message form added

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '9d359c23-34b9-472f-b618-c369236c1324';
 
 const Modal = () => {
   const [username, setUsername] = useState('');
